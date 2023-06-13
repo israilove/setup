d4f56b1e37648899656de2b65e20d79221753845
Bek <beksultan.israiluulu9@gmail.com>
2023-06-13 12:40:17 +0600

    setup #3

--- core/urls.py
+++ core/urls.py
@@ -22,13 +22,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Shop API",
@@ -38,3 +31,12 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=[permissions.AllowAny],
 )
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
